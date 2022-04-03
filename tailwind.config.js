f24387fcba65d3bfb1db6ccb7ad645e351f5841e
Nick Hellemans <NickHellemans93@gmail.com>
2022-04-03 13:05:33 +0200

    Add welcome page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './layouts/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './content/**/*.mdx',
   ],
   theme: {
     extend: {
