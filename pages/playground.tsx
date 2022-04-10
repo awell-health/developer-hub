5aca1d2bc363beae29a5b8f55ae1d132055139e4
Nick Hellemans <NickHellemans93@gmail.com>
2022-04-10 23:58:28 +0200

    chore(env): aPI key should come from env variable

--- pages/playground.tsx
+++ pages/playground.tsx
@@ -10,13 +10,17 @@ export default function Playground() {
     throw new Error('NEXT_PUBLIC_SANDBOX_GRAPHQL_API_URL is not defined')
   }
 
+  if (!process.env.NEXT_PUBLIC_SANDBOX_GRAPHQL_API_KEY) {
+    throw new Error('NEXT_PUBLIC_SANDBOX_GRAPHQL_API_KEY is not defined')
+  }
+
   // const GraphiQL = require('graphiql')
   // const { createGraphiQLFetcher } = require('@graphiql/toolkit')
 
   const fetcher = createGraphiQLFetcher({
     url: process.env.NEXT_PUBLIC_SANDBOX_GRAPHQL_API_URL,
     headers: {
-      apiKey: 'I0xHR9QOuOPhupv68kdKfVeR3CwIFkIe',
+      apiKey: process.env.NEXT_PUBLIC_SANDBOX_GRAPHQL_API_KEY,
     },
   })
 
