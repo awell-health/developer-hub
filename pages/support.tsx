88c1f6ec9f69643b79ade04e511127412dea1cb9
Nick Hellemans <NickHellemans93@gmail.com>
2022-04-14 14:36:19 +0200

    docs(support): add content

--- pages/support.tsx
+++ pages/support.tsx
@@ -7,10 +7,42 @@ import { SEO } from '../src/components/SEO'
 export default function Community() {
   return (
     <>
-      <SEO title="Support" url={`/support`} canonicalUrl={`/support`} />
-      <DocsHeader heading="Support" title="We are here to help you!" />
-      <div id="content-wrapper">
-        <p>Hey, we are currently working on this page!</p>
+      <div className="max-w-3xl mx-auto xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16">
+        <SEO title="Support" url={`/support`} canonicalUrl={`/support`} />
+        <DocsHeader heading="Support" title="Help when you need it" />
+        <div id="content-wrapper">
+          <p>
+            We truely value developer experience and are constantly updating our
+            documentation. {`Don't`} hesitate to get in touch with us directly
+            for:
+          </p>
+          <ul>
+            <li>
+              <u>Documentation:</u> missing or incorrect documentation,
+              suggestions, or maybe additional material you would like to see
+            </li>
+            <li>
+              <u>Questions & advice:</u> send us a message via Intercom
+            </li>
+            <li>
+              <u>Bugs & missing features:</u> if you have issues you believe are
+              due to a bug in the API itself or a missing feature, you can also
+              send us a message via Intercom
+            </li>
+          </ul>
+          <blockquote>
+            <p>
+              <strong>Tip:</strong> If you {`didn't`} find what you were looking
+              for, try searching the docs first.
+            </p>
+          </blockquote>
+          <h2 id="intercom">Intercom</h2>
+          <p>
+            Get fast, helpful support via our chat. Send us a direct message via
+            Intercom (bottom right corner of the screen) and we will get back to
+            you asap.
+          </p>
+        </div>
       </div>
     </>
   )
