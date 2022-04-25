d3c01183b79763fa40a8335962fdbef88d12b682
Nick Hellemans <NickHellemans93@gmail.com>
2022-04-25 12:47:02 +0200

    fix(typo): sEO description

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -4,5 +4,5 @@ export const SEO: SEOType = {
   rootUrl: 'https://developers.awellhealth.com',
   title: 'Awell Developers',
   siteName: 'Awell Developers',
-  description: 'Orchestrate clinical worksflows at Scale with Awell Health',
+  description: 'Orchestrate clinical workflows at scale with Awell Health',
 }
