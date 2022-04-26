e7c9b73a21517e0f613c1dd48a92d9676fa377b8
Nick Hellemans <NickHellemans93@gmail.com>
2022-04-26 18:11:56 +0200

    test(webhooks): return JSON response

--- pages/api/webhooks/awell-orchestration.ts
+++ pages/api/webhooks/awell-orchestration.ts
@@ -3,8 +3,9 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     console.log(req.body)
-    return res.status(200).send({ message: 'Webhook received.' })
+    return res.status(200).json({ message: 'Webhook received.' })
   } else {
-    return res.status(405).send({ message: 'Method is not allowed.' })
+    res.setHeader('Allow', 'POST')
+    return res.status(405).json({ message: 'Method is not allowed.' })
   }
 }
