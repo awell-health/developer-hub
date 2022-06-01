a283119e9d5c9ac9e9a404527b4e3b31dd94cbd0
Nick Hellemans <NickHellemans93@gmail.com>
2022-06-01 16:54:18 +0200

    docs: playground button should be visible on all api-reference pages

--- pages/api-reference/[...slug].tsx
+++ pages/api-reference/[...slug].tsx
@@ -61,7 +61,7 @@ export default function ApiPage({
           postman={{
             showPostmanButton: frontMatter.showPostmanButton !== false,
           }}
-          playgroundButton={frontMatter.showPlaygroundButton}
+          playgroundButton={true}
         />
       )}
       <div>
