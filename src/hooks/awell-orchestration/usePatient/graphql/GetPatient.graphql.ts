60ccee032753a66d6862355ed8a66a7640922d56
Nick Hellemans <NickHellemans93@gmail.com>
2022-07-01 17:40:16 +0200

    feat(hooks): createPatient & updatePatient hook

--- src/hooks/awell-orchestration/usePatient/graphql/GetPatient.graphql.ts
+++ src/hooks/awell-orchestration/usePatient/graphql/GetPatient.graphql.ts
@@ -4,7 +4,27 @@ export const GET_PATIENT = gql`
   query GetPatient($patient_id: String!) {
     patient(id: $patient_id) {
       patient {
-        ...Patient
+        id
+        profile {
+          email
+          sex
+          birth_date
+          first_name
+          last_name
+          name
+          phone
+          mobile_phone
+          preferred_language
+          patient_code
+          national_registry_number
+          address {
+            street
+            city
+            zip
+            state
+            country
+          }
+        }
       }
     }
   }
