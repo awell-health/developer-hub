1b88ef70e55251e7bfae14c4f449cb37b3640473
Nick Hellemans <NickHellemans93@gmail.com>
2022-07-20 13:47:23 +0200

    style(stackselector): better dark styles

--- src/components/StackSelector/atoms/CardSelector.tsx
+++ src/components/StackSelector/atoms/CardSelector.tsx
@@ -28,9 +28,9 @@ export const CardSelector = ({
                   !option.disabled
                     ? 'cursor-pointer focus:outline-none'
                     : 'opacity-25 cursor-not-allowed',
-                  active ? 'ring-1 ring-sky-400' : '',
+                  active ? '' : '',
                   checked
-                    ? 'text-blue-600 border-2 border-blue-600 dark:text-blue-500 border-blue-500'
+                    ? 'text-blue-600 border-2 border-blue-600 dark:text-sky-300 dark:border-sky-400'
                     : 'bg-white dark:bg-transparent dark:text-slate-100 sborder-slate-400 text-slate-900 hover:bg-gray-50',
                   'border rounded-md py-1 px-2 flex items-center justify-center text-sm font-semibold uppercase sm:flex-1'
                 )
