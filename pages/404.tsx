d10e0fd7f8c0a09ddb06448686a47ed787d89046
Nick Hellemans <NickHellemans93@gmail.com>
2022-07-20 11:39:03 +0200

    feat(dark-styles): for 404 and coming soon page

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,19 +5,19 @@ import { SEO } from '../src/components/SEO'
 
 export default function Custom404() {
   return (
-    <div className="h-screen pt-16 pb-12 flex flex-col bg-white">
+    <div className="h-screen pt-16 pb-12 flex flex-col">
       <SEO title="404" url="" preventCrawling={true} canonicalUrl="" />
       <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="py-16">
           <div className="text-center">
-            <p className="text-sm font-semibold text-blue-600 uppercase tracking-wide">
+            <p className="text-sm font-semibold text-blue-600 uppercase tracking-wide dark:text-sky-400">
               404
             </p>
             <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
               Page not found.
             </h1>
             <div className="w-96 mx-auto">
-              <p className="mt-2 text-base text-gray-500">
+              <p className="mt-2 text-base text-gray-500 dark:text-slate-400">
                 It seems you are lost. Go back to our home page or use the
                 search bar to try and find what you are looking for.
               </p>
@@ -27,7 +27,7 @@ export default function Custom404() {
             </div>
             <div className="mt-6">
               <Link href="/">
-                <a className="text-base font-medium text-blue-600 hover:text-blue-500">
+                <a className="text-base font-medium text-blue-600 dark:text-sky-400 hover:text-blue-500 dark:hover:text-sky-500">
                   Go back home<span aria-hidden="true"> &rarr;</span>
                 </a>
               </Link>
--- pages/coming-soon.tsx
+++ pages/coming-soon.tsx
@@ -5,19 +5,19 @@ import { SEO } from '../src/components/SEO'
 
 export default function Custom404() {
   return (
-    <div className="h-screen pt-16 pb-12 flex flex-col bg-white">
+    <div className="h-screen pt-16 pb-12 flex flex-col">
       <SEO title="Coming soon" url="/coming-soon" canonicalUrl="/coming-soon" />
       <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="py-16">
           <div className="text-center">
-            <p className="text-sm font-semibold text-blue-600 uppercase tracking-wide">
+            <p className="text-sm font-semibold text-blue-600 dark:text-sky-400 uppercase tracking-wide">
               Thank you for your interest
             </p>
             <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
               This page is coming soon
             </h1>
             <div className="w-96 mx-auto">
-              <p className="mt-2 text-base text-gray-500">
+              <p className="mt-2 text-base text-gray-500 dark:text-slate-400">
                 We are currently working on this page. In the meantime, go back
                 to our home page or use the search bar to try and find other
                 documentation that could help you.
@@ -28,7 +28,7 @@ export default function Custom404() {
             </div>
             <div className="mt-6">
               <Link href="/">
-                <a className="text-base font-medium text-blue-600 hover:text-blue-500">
+                <a className="text-base font-medium text-blue-600 dark:text-sky-400 hover:text-blue-500 dark:hover:text-sky-500">
                   Go back home<span aria-hidden="true"> &rarr;</span>
                 </a>
               </Link>
