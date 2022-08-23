5d86f31b6a0b17143d3a19ac4c40bd9f84d822c1
Nick Hellemans <NickHellemans93@gmail.com>
2022-08-23 23:36:32 +0200

    chore: add manual search indeces for hosted pathwya, activity and custom integration

--- scripts/build-algolia-search/manual-indexes.js
+++ scripts/build-algolia-search/manual-indexes.js
@@ -34,6 +34,36 @@ const manualIndexes = [
       'Learn how to set up and deploy a webhook to listen to events from Awell! Select the webhooks you would like to listen to and copy/paste the corresponding code',
     slug: 'awell-orchestration/developer-tools/webhook-builder',
   },
+  {
+    objectID: 'awell-orchestration/docs/activities/hosted-pathway',
+    space: 'Awell Orchestration',
+    title: 'Hosted pathway',
+    description:
+      'Get up and running with Awell Orchestration in less than a day',
+    content:
+      'The quickest way to get up and running with Awell is the "Awell-hosted pathway page". This page will allow users to complete a pathway or flow and is a low-code integration.',
+    slug: 'awell-orchestration/docs/activities/hosted-pathway',
+  },
+  {
+    objectID: 'awell-orchestration/docs/activities/hosted-activity',
+    space: 'Awell Orchestration',
+    title: 'Hosted activity',
+    description:
+      'Redirect stakeholders to an Awell Hosted Activity page to complete activities',
+    content:
+      'This integration enables you to orchestrate activities over time for multiple stakeholders on an Awell Hosted Activity page.',
+    slug: 'awell-orchestration/docs/activities/hosted-activity',
+  },
+  {
+    objectID: 'awell-orchestration/docs/activities/custom-integration',
+    space: 'Awell Orchestration',
+    title: 'Custom integration',
+    description:
+      'Build your own UI on Awell Orchestration for full customization and flexibility.',
+    content:
+      'If you want full flexibility on how things look and feel, building a custom integration would be your best bet. To get you started, we prepared a couple of stories that could help you get started with building things on top of our API.',
+    slug: 'awell-orchestration/docs/activities/custom-integration',
+  },
 ]
 
 module.exports = manualIndexes
