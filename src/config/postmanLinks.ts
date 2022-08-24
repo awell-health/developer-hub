26bfa5a493bea46521b0bbec5f5cc8eb8c295c41
Nick Hellemans <NickHellemans93@gmail.com>
2022-08-24 11:27:40 +0200

    fix: typo

--- src/config/postmanLinks.ts
+++ src/config/postmanLinks.ts
@@ -5,7 +5,7 @@ export const postManLinks = [
     heapTracker: 'download-postman-web-collection',
   },
   {
-    title: 'Download Postman collection for the Windows/Mac',
+    title: 'Download Postman collection for Windows/Mac',
     url: 'postman://app/collections/import/19166134-6ea42ef7-a67f-43a6-b2da-f8ab0b02b5cc-UVXnGEJb?referrer=https%3A%2F%2Fdocumenter.getpostman.com%2Fview%2F19166134%2FUVXnGEJb&versionTag=latest#?',
     heapTracker: 'download-postman-mac-windows-collection',
   },
