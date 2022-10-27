2b38519e11b1944cf8c99b24e74905b504cb10d0
Nick Hellemans <NickHellemans93@gmail.com>
2022-10-27 10:47:57 +0200

    refactor: clinical workflow --> care flow

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -4,5 +4,5 @@ export const SEO: SEOType = {
   rootUrl: 'https://developers.awellhealth.com',
   title: 'Developer Hub',
   siteName: 'Developer Hub',
-  description: 'Orchestrate clinical workflows at scale with Awell Health',
+  description: 'Orchestrate care flows at scale with Awell Health',
 }
