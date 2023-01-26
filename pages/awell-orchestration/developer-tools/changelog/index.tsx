26ecce50b4ab4abd2eff7501732e84953c30ba2f
Rahul Keerthi <rahulkeerthi2@gmail.com>
2023-01-26 18:25:53 +0000

    docs(api): add form field to GetPathwayActivities query docs and changelog

--- pages/awell-orchestration/developer-tools/changelog/index.tsx
+++ pages/awell-orchestration/developer-tools/changelog/index.tsx
@@ -54,26 +54,26 @@ export default function ChangelogPage({ releases }: ChangelogPageProps) {
       </div>
       <div className="grid gap-3 sm:gap-6 mx-auto grid-cols-2 sm:grid-cols-3">
         {releases.map((release) => (
-          <div
-            key={release.slug}
-            className="p-6 w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
-          >
-            <a href="#">
+          <a
+            href={`changelog/${release.slug}`}
+            key={release.slug} >
+            <div
+              className="p-6 w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+            >
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 v{release.frontMatter.title}
               </h5>
-            </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Released on{' '}
-              {format(new Date(release.frontMatter.releaseDate), 'MMMM d, y')}
-            </p>
-            <a
-              href={`changelog/${release.slug}`}
-              className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-            </a>
-          </div>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Released on{' '}
+                {format(new Date(release.frontMatter.releaseDate), 'MMMM d, y')}
+              </p>
+              <div
+                className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+              </div>
+            </div>
+          </a>
         ))}
       </div>
       <div className="mt-12 max-w-3xl mx-auto xl:max-w-none xl:ml-0 xl:mr-[15.5rem] xl:pr-16">
@@ -93,8 +93,8 @@ export const getStaticProps: GetStaticProps = async () => {
     return a.frontMatter.releaseDate < b.frontMatter.releaseDate
       ? 1
       : a.frontMatter.releaseDate > b.frontMatter.releaseDate
-      ? -1
-      : 0
+        ? -1
+        : 0
   })
 
   return {
