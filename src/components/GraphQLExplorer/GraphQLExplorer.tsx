0dd8cc00855df6dad575e024c9c74f96c8acf50f
Nick Hellemans <NickHellemans93@gmail.com>
2023-02-06 13:25:52 +0100

    fix(api-explorer): test same fix as playground

--- src/components/GraphQLExplorer/GraphQLExplorer.tsx
+++ src/components/GraphQLExplorer/GraphQLExplorer.tsx
@@ -1,35 +1,18 @@
 import {} from 'graphiql'
-import { GraphQLError, GraphQLSchema } from 'graphql'
-import dynamic from 'next/dynamic'
-import { useEffect, useState } from 'react'
 
 import { useGraphQLSchema } from '@/hooks/useGraphQLSchema'
 
-/**
- * The graphiql library can only be rendered on the client side.
- * SSR is not supported.
- */
-const DocExplorer = dynamic<{
-  schema?: GraphQLSchema | null
-  schemaErrors?: readonly GraphQLError[]
-}>(() => import('graphiql').then((lib) => lib.DocExplorer), {
-  ssr: false,
-})
-
 export const GraphQLExplorer = () => {
-  const [isClientRendering, setIsClientRendering] = useState(false)
   const { schema } = useGraphQLSchema()
+  // https://github.com/graphql/graphiql/issues/118
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { DocExplorer } = require('graphiql')
 
-  /**
-   * See https://github.com/graphql/graphiql/issues/2368
-   */
-  useEffect(() => {
-    setIsClientRendering(true)
-  }, [])
+  if (typeof window === 'undefined') return null
 
-  return isClientRendering ? (
+  return (
     <div id="graphiql-embed">
       <DocExplorer schema={schema} />
     </div>
-  ) : null
+  )
 }
