acebb224f664f07308a500ddb4e9e6c875854a88
Nick Hellemans <NickHellemans93@gmail.com>
2023-02-23 18:15:46 +0100

    style: hide plugin space for now

--- src/config/spaces.ts
+++ src/config/spaces.ts
@@ -1,6 +1,6 @@
 import { Space } from '@/types/space.types'
 
-export const spaces: { label: string; path: string }[] = [
+export const spaces: { label: string; path: string; active?: boolean }[] = [
   {
     label: 'Awell Orchestration',
     path: `/${Space.AWELL_ORCHESTRATION}`,
@@ -8,6 +8,7 @@ export const spaces: { label: string; path: string }[] = [
   {
     label: 'Awell Plugins',
     path: `/${Space.AWELL_PLUGINS}/docs/getting-started/what-are-awell-plugins`,
+    active: false,
   },
   {
     label: 'Awell Score',
