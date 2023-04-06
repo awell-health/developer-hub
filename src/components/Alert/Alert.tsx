ab0988914d8da12e951cefa5d92e393a47a8123a
Etienne Bömcke <etienne@awellhealth.com>
2023-04-06 14:26:11 +0200

    feat(data-repository): add explanation about qualified keys
    - explain how qualified keys are generated and add an example for
    the BMI form
    - fix alignment issue in Alert component when title is not defined

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -15,10 +15,12 @@ export const Alert = ({ type, title, children }: AlertProps) => {
       <div className="alert flex rounded-3xl p-6 bg-sky-50 dark:bg-slate-800/60 dark:ring-1 dark:ring-slate-300/10">
         <Info />
         <div className="ml-4 flex-auto">
-          <p className="m-0 font-display text-xl text-sky-900 dark:text-sky-400">
-            {title}
-          </p>
-          <div className="text-lg prose mt-2.5 text-sky-800 [--tw-prose-background:theme(colors.sky.50)] prose-a:text-sky-900 prose-code:text-sky-900 dark:text-slate-300 dark:prose-code:text-slate-300">
+          {title && title !== "" && (
+            <p className="m-0 mb-2.5 font-display text-xl text-sky-900 dark:text-sky-400">
+              {title}
+            </p>
+          )}
+          <div className="text-lg prose text-sky-800 [--tw-prose-background:theme(colors.sky.50)] prose-a:text-sky-900 prose-code:text-sky-900 dark:text-slate-300 dark:prose-code:text-slate-300">
             {children}
           </div>
         </div>
@@ -30,10 +32,12 @@ export const Alert = ({ type, title, children }: AlertProps) => {
     <div className="alert my-8 flex rounded-3xl p-6 bg-amber-50 dark:bg-slate-800/60 dark:ring-1 dark:ring-slate-300/10">
       <Warning />
       <div className="ml-4 flex-auto">
-        <p className="m-0 font-display text-xl text-amber-900 dark:text-amber-500">
-          {title}
-        </p>
-        <div className="prose mt-2.5 text-amber-800 [--tw-prose-underline:theme(colors.amber.400)] [--tw-prose-background:theme(colors.amber.50)] prose-a:text-amber-900 prose-code:text-amber-900 dark:text-slate-300 dark:[--tw-prose-underline:theme(colors.sky.700)] dark:prose-code:text-slate-300">
+        {title && title !== "" && (
+          <p className="m-0 mb-2.5 font-display text-xl text-amber-900 dark:text-amber-500">
+            {title}
+          </p>
+        )}
+        <div className="prose text-amber-800 [--tw-prose-underline:theme(colors.amber.400)] [--tw-prose-background:theme(colors.amber.50)] prose-a:text-amber-900 prose-code:text-amber-900 dark:text-slate-300 dark:[--tw-prose-underline:theme(colors.sky.700)] dark:prose-code:text-slate-300">
           {children}
         </div>
       </div>
