93eb6e7d19c4b69e68fd7276bc9f24c897945043
Rahul Keerthi <rahulkeerthi2@gmail.com>
2023-10-05 17:35:52 +0100

    docs(postman): update postman links

--- src/config/postmanLinks.ts
+++ src/config/postmanLinks.ts
@@ -1,12 +1,12 @@
 export const postManLinks = [
   {
     title: 'Download Postman collection for the Web',
-    url: 'https://elements.getpostman.com/view/import?collection=19166134-6ea42ef7-a67f-43a6-b2da-f8ab0b02b5cc-UVXnGEJb&referrer=https%3A%2F%2Fdocumenter.getpostman.com%2Fview%2F19166134%2FUVXnGEJb&versionTag=latest',
+    url: 'https://elements.getpostman.com/view/import?collection=19166134-9d27906d-6a02-4812-8e09-39c58b4236d0&referrer=https%3A%2F%2Fdocumenter.getpostman.com%2Fview%2F19166134%2FUVXnGEJb&versionTag=latest',
     heapTracker: 'download-postman-web-collection',
   },
   {
-    title: 'Download Postman collection for Windows/Mac',
-    url: 'postman://app/collections/import/19166134-6ea42ef7-a67f-43a6-b2da-f8ab0b02b5cc-UVXnGEJb?referrer=https%3A%2F%2Fdocumenter.getpostman.com%2Fview%2F19166134%2FUVXnGEJb&versionTag=latest#?',
+    title: 'Download Postman collection for standalone Postman app',
+    url: 'postman://app/collections/import/19166134-9d27906d-6a02-4812-8e09-39c58b4236d0?referrer=https%3A%2F%2Fdocumenter.getpostman.com%2Fview%2F19166134%2FUVXnGEJb&versionTag=latest#?',
     heapTracker: 'download-postman-mac-windows-collection',
   },
 ]
