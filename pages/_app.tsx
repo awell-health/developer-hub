834fc329aa393fc8c619180c35a11c5878755767
Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>
2023-10-18 16:01:24 +0200

    chore(): optimize performance for SEO pt2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -96,7 +96,7 @@ const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
       />
       <Script
         id="intercom_2"
-        data-partytown-config
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `
             (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/yfwvlb21';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
